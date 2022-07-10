95bade6f88b1d7e3a12b28cd61691aa631316ba9
yuya.mori <yuuya421@gmail.com>
2022-07-10 18:59:31 +0900

    NewServlet.javaを作成

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,7 +1,7 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
+import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -14,16 +14,16 @@ import models.Message;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class NewServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public NewServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -33,11 +33,26 @@ public class IndexServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
+        em.getTransaction().begin();
 
-        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
-        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+        // Messageのインスタンスを生成
+        Message m = new Message();
+
+        String content = "hello";
+        m.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
+        m.setCreated_at(currentTime);
+        m.setUpdated_at(currentTime);
+
+        // データベースに保存
+        em.persist(m);
+        em.getTransaction().commit();
+
+        // 自動採番されたIDの値を表示
+        response.getWriter().append(Integer.valueOf(m.getId()).toString());
 
         em.close();
-    }
+     }
 
 }
--- src/models/Message.java
+++ src/models/Message.java
@@ -7,10 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tasks")
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllMessages",
+        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+    )
+})
 public class Message {
     @Id
     @Column(name = "id")
