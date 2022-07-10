a9cf2d7d5b14582003e554c487fe9a453f9db9e2
yuya.mori <yuuya421@gmail.com>
2022-07-10 22:18:57 +0900

    フラッシュメッセージを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -42,6 +42,11 @@ public class IndexServlet extends HttpServlet {
 
         request.setAttribute("messages", messages);
 
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
         rd.forward(request, response);
     }
